9dc5a15cabb39d9d1a89f45e6417d4bc8ae45ad6
lhmet <lhmet@ufsm.br>
2018-02-13 22:31:07 -0200

    pck doc

---
+++ R/sib2r.R
@@ -0,0 +1,16 @@
+#' sib2r: A R interface to the Simple Biosphere Model 2.0 (SiB2).
+#'
+#'  A multi-OS and stand-alone version of the SiB2 model.
+#'
+#' @section inmetr functions:
+#' The only function you're likely to need from \pkg{sib2r} is
+#' \itemize{
+#'    \item \code{\link{call_sib2}}  
+#' }
+#'  The data included in \code{sib2r} is available at:
+#' \itemize{
+#'    \item \file{sib2r/inst/extdata/data2}  
+#' }
+#' @docType package
+#' @name sib2r
+NULL
